db4fbdae671e24eca76e8421a3d29e94fd1e849c
Pao <pao@paos-mbp.dynamic.ucsd.edu>
2016-08-22 13:46:27 -0700

    add 1 file

---
+++ parse.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+# tried
+#  $f="asfd-de-Van";
+#    $f =~ s/-/ /g ;   #sub - by blank
+
+$f="<title>Title</title><p>";
+$f="<p></p>\n";   #s/^<p><\/p>/new/
+$f="</u>TY  - JOUR<p>";
+
+print "$f \n";
+
+       if ($f =~ m/^<\/u>/) {
+         $f =~ s/^<\/u>//;
+        print "got it - $f -\n";
+     }
+
+#print "$f \n";
+
+
